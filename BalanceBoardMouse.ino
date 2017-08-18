ca8b724a11a09807d37d19e8d439fe67bc3162b2
Josef Fällman <josef.fallman@gmail.com>
2017-08-18 13:54:21 +0200

    Project setup and start

---
+++ BalanceBoardMouse.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		BalanceBoardMouse.ino
+ Created:	8/18/2017 1:25:55 PM
+ Author:	Josef
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
